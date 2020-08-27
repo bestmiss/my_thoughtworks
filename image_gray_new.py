ce0991782add4d41ea0fcfb672cb456f0ffd266a
my-code668 <15896540971@163.com>
2020-08-27 10:51:16 +0800

    numpy基础最新

--- image_gray_new.py
+++ image_gray_new.py
@@ -1,10 +1,13 @@
 from PIL import Image
 import numpy as np
 #读取彩色图片
-image = Image.open('image.jpg')
-image.show()
-image_array = np.array(image)
+image = Image.open('image.jpg') 
+#image.show() 
+image_ar = np.array(image)
+image_array=np.mean(image_ar, axis=2)
 #灰度图
-image1 = Image.fromarray(image_array)
-image1.show()
-
+image_gray = Image.fromarray(image_array)
+image_gray.show()
+if image_gray.mode == "F":
+    image_gray = image_gray.convert('RGB') 
+image_gray.save('image_gray.jpg')
